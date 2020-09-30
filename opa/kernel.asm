6b2ebf9d7240f75bb9b417b6e89ad5b9a9f63d03
Pedro Cunha <pvcunh4@gmail.com>
2020-09-29 22:39:52 -0300

    time ok

--- opa/kernel.asm
+++ opa/kernel.asm
@@ -2,9 +2,10 @@ org 0x7e00
 jmp 0x0000:start
 
 data:
-	ball_x equ 10    ;pos x
-    ball_y equ 10    ;pos y
+	ball_x dw 10    ;pos x
+    ball_y dw 10    ;pos y
     ball_size equ 4     ;tamanho da bola
+    aux db 0      ;vai checar o tempo
 
 
 set_video_mode:
@@ -17,10 +18,20 @@ set_video_mode:
     int 10h         ;executa a conf
     ret
 
+%macro delay 2
+	pusha
+	mov cx, %1
+	mov dx, %2
+	
+	mov ah, 86h
+	int 15h
+	popa
+%endmacro
+
 
 draw_ball:
-    mov cx, ball_x  ;determina pos inicial da coluna (x)
-    mov dx, ball_y  ;determina posinicial da linha (y)
+    mov cx, [ball_x]  ;determina pos inicial da coluna (x)
+    mov dx, [ball_y] ;determina posinicial da linha (y)
 
     horizontal_loop:
         mov ah, 0Ch     ;coloca no modo de escrever pixel
@@ -31,14 +42,14 @@ draw_ball:
         inc cx          ;incrementa coluna
         
         mov ax, cx      ; se cx - pos_x inicial > ball_size então uma linha foi completada 
-        sub ax, ball_x
+        sub ax, [ball_x]
         cmp ax, ball_size
         jng horizontal_loop ; se a comparacao nao for maior ele continua o loop horizontal (jump if not greater)
         
-        mov cx, ball_x
+        mov cx, [ball_x]
         inc dx              ; se dx - ball_y > ball_size the drawing is complete
         mov ax, dx
-        sub ax, ball_y
+        sub ax, [ball_y]
         cmp ax, ball_size
         jng horizontal_loop     ;se nao for maior passamos para a prox coluna
 
@@ -53,8 +64,15 @@ start:
     
     ;Código do projeto...
     call set_video_mode
-    call draw_ball
 
+    check_time:
+        call draw_ball
+    
+        mov bx, [ball_x]
+        inc bx
+        mov [ball_x], bx
+        delay 1, 100
+        jmp check_time
 
 
 jmp $
