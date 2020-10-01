ac1cc538304289a83e1f25337049236acf4e69b7
Gabriel Oliveira <gthings2023@gmail.com>
2020-10-01 00:39:13 -0300

    barras com limites

--- opa/kernel.asm
+++ opa/kernel.asm
@@ -10,11 +10,24 @@ data:
     ball_y dw 0    ;pos y
     ball_size equ 4     ;tamanho da bola
     
+    bar_left_init_x dw 0
+    bar_right_init_x dw 306
+    ball_left_init_y dw 20
+
+    bar_left_x dw 0    ;pos x
+    bar_left_y dw 80    ;pos y
+
+    bar_right_x dw 306    ;pos x
+    bar_right_y dw 80    ;pos y
+    bar_sizex equ 4     ;tamanho da bola 
+    bar_sizey equ 40
+
     pad_size_x equ 3 ; tamanho do pad em x
     pad_size_y equ 9 ; tamanho do pad em y
     
     aux db 0      ;vai checar o tempo
 
+    bar_vel dw 5
     bs_x dw 2
     bs_y dw 2
     flag_x dw 1
@@ -95,6 +108,145 @@ draw_ball:
 
     ret
 
+draw_bar_left:
+    mov cx, [bar_left_x]  ;determina pos inicial da coluna (x)
+    mov dx, [bar_left_y] ;determina posinicial da linha (y)
+
+    horizontal_loop1:
+        mov ah, 0Ch     ;coloca no modo de escrever pixel
+        mov al, 15      ;determina a cor do pixel
+        mov bh, 00h     ;determina o numero da pagina
+        int 10h
+        
+        inc cx          ;incrementa coluna
+        
+        mov ax, cx      ; se cx - pos_x inicial > ball_size então uma linha foi completada 
+        sub ax, [bar_left_x]
+        cmp ax, bar_sizex
+        jng horizontal_loop1 ; se a comparacao nao for maior ele continua o loop horizontal (jump if not greater)
+        
+        mov cx, [bar_left_x]
+        inc dx              ; se dx - ball_y > ball_size the drawing is complete
+        mov ax, dx
+        sub ax, [bar_left_y]
+        cmp ax, bar_sizey
+        jng horizontal_loop1     ;se nao for maior passamos para a prox coluna
+
+    ret
+
+draw_bar_right:
+    mov cx, [bar_right_x]  ;determina pos inicial da coluna (x)
+    mov dx, [bar_right_y] ;determina posinicial da linha (y)
+
+    horizontal_loop2:
+        mov ah, 0Ch     ;coloca no modo de escrever pixel
+        mov al, 15      ;determina a cor do pixel
+        mov bh, 00h     ;determina o numero da pagina
+        int 10h
+        
+        inc cx          ;incrementa coluna
+        
+        mov ax, cx      ; se cx - pos_x inicial > ball_size então uma linha foi completada 
+        sub ax, [bar_right_x]
+        cmp ax, bar_sizex
+        jng horizontal_loop2 ; se a comparacao nao for maior ele continua o loop horizontal (jump if not greater)
+        
+        mov cx, [bar_right_x]
+        inc dx              ; se dx - ball_y > ball_size the drawing is complete
+        mov ax, dx
+        sub ax, [bar_right_y]
+        cmp ax, bar_sizey
+        jng horizontal_loop2     ;se nao for maior passamos para a prox coluna
+
+    ret
+
+move_bar:
+    mov ah,01h
+    int 16h
+    jz  bar_right_check_movement ; zf = 1, jz-> jump if zero
+
+    ;verificando qual key foi pressionada
+    mov ah,00h
+    int 16h
+    cmp al,77h 
+    je bar_left_move_up
+
+    ;verificando qual key foi pressionada
+    cmp al,73h 
+    je bar_left_move_down
+    jmp bar_right_check_movement
+
+    bar_left_move_up:
+        mov ax,[bar_vel]
+        sub [bar_left_y],ax
+
+        mov ax,3
+        cmp [bar_left_y],ax
+        jl fix_padle_left_top
+        jmp bar_right_check_movement
+
+        fix_padle_left_top:
+            mov ax,3
+            mov [bar_left_y],ax
+            jmp bar_right_check_movement
+
+
+    bar_left_move_down:
+        mov ax,[bar_vel]
+        add [bar_left_y],ax
+        mov ax,200
+        sub ax,3
+        sub ax,bar_sizey
+        cmp [bar_left_y],ax
+        jg  fix_bar_left_bottom
+        jmp bar_right_check_movement
+
+        fix_bar_left_bottom:
+            mov [bar_left_y],ax
+            jmp exit_mov
+
+    bar_right_check_movement:
+
+        cmp al,6Fh 
+        je bar_right_move_up
+
+        ;verificando qual key foi pressionada
+        cmp al,6Ch 
+        je bar_right_move_down
+        jmp exit_mov
+
+        bar_right_move_up:
+            mov ax,[bar_vel]
+            sub [bar_right_y],ax
+
+            mov ax,3
+            cmp [bar_right_y],ax
+            jl fix_padle_right_top
+            jmp exit_mov
+
+            fix_padle_right_top:
+                mov ax,3
+                mov [bar_right_y],ax
+                jmp exit_mov
+
+        bar_right_move_down:
+            mov ax,[bar_vel]
+            add [bar_right_y],ax
+        
+            mov ax,200
+            sub ax,3
+            sub ax,bar_sizey
+            cmp [bar_right_y],ax
+            jg fix_padle_right_bottom
+            jmp exit_mov
+
+            fix_padle_right_bottom:
+                mov [bar_right_y],ax
+                jmp exit_mov
+
+    exit_mov:
+
+        ret
 
 collision:
     check_right_collision:
@@ -176,10 +328,17 @@ start:
     call ball_reset
     check_time:
         call draw_ball
+
+        call draw_bar_left
+
+        call draw_bar_right
         
         call walk_x ;anda em x
 
         call walk_y ;anda em y
+
+        call move_bar
+        
 
         call collision
 
